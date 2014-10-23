1e4fcffe5373cd7de5c787a8ecb4a06863680a43
Abiy Seifu <abiy@ripple.com>
2014-10-23 16:54:01 -0700

    [TEST] send and confirm payment

--- test/sendAndConfirmPayment.js
+++ test/sendAndConfirmPayment.js
@@ -1,8 +1,7 @@
-var Client = require('../');
-var assert = require('assert');
-var request = require('request');
-var sinon = require('sinon');
-var uuid = require('node-uuid');
+const Client = require('../');
+const assert = require('assert');
+const fixtures = require('./fixtures');
+const uuid = require('node-uuid');
 
 var SECRET = process.env.RIPPLE_ACCOUNT_SECRET;
 
@@ -17,11 +16,12 @@ describe('Ripple REST Client sendPayment', function() {
 
   if(SECRET){
     it('should send and confirm payment via the response payment status url', function(done){
+      this.timeout(10000);
       payment = {
-        source_account: 'rMinhWxZz4jeHoJGyddtmwg6dWhyqQKtJz',
-        source_amount: { value: '.005', currency: 'XRP', issuer: '' },
-        destination_account: 'ra9EVPRsiqncEfrRpJudDV34AqxFao8Zv9',
-        destination_amount: { value: '1', currency: 'XRP', issuer: '' },
+        source_account: fixtures.ripple_address.source_account,
+        source_amount: { value: '.005', currency: 'SWD', issuer: '' },
+        destination_account: fixtures.ripple_address.destination_account,
+        destination_amount: { value: '1', currency: 'SWD', issuer: fixtures.ripple_address.source_account },
         partial_payment: false,
         no_direct_ripple: false,
         destination_tag: '0'
@@ -34,9 +34,6 @@ describe('Ripple REST Client sendPayment', function() {
       };
 
       client.sendAndConfirmPayment(paymentObj, function(error, response){
-        console.log('err',  error);
-        console.log('response', response);
-        assert.notStrictEqual(response, null);
         assert.strictEqual(response.source_account, payment.source_account);
         assert.strictEqual(response.result, 'tesSUCCESS');
         done();
@@ -45,10 +42,11 @@ describe('Ripple REST Client sendPayment', function() {
     });
 
     it('should successfully send and confirm a non-XRP payment', function(done){
+      this.timeout(10000);
       nonXRPayment = {
-        source_account: 'rMinhWxZz4jeHoJGyddtmwg6dWhyqQKtJz',
+        source_account: fixtures.ripple_address.source_account,
         source_amount: { value: '1', currency: 'SWD', issuer: '' },
-        destination_account: 'rscJF4TWS2jBe43MvUomTtCcyrbtTRMSNr',
+        destination_account: fixtures.ripple_address.destination_account,
         destination_amount: { value: '1', currency: 'SWD', issuer: 'rMinhWxZz4jeHoJGyddtmwg6dWhyqQKtJz' },
         partial_payment: false,
         no_direct_ripple: false,
@@ -62,8 +60,6 @@ describe('Ripple REST Client sendPayment', function() {
       };
 
       client.sendAndConfirmPayment(nonXRPaymentObj, function(error, response){
-        console.log('err',  error);
-        console.log('response', response);
         assert.strictEqual(response.source_account, payment.source_account);
         assert.strictEqual(response.result, 'tesSUCCESS');
         assert.strictEqual(response.destination_amount.currency, nonXRPayment.destination_amount.currency);
@@ -72,7 +68,7 @@ describe('Ripple REST Client sendPayment', function() {
     });
 
     it('should fail because destination account and amount is missing', function(done){
-      this.timeout(5000);
+      this.timeout(10000);
       faildPayment = {
         source_account: 'rMinhWxZz4jeHoJGyddtmwg6dWhyqQKtJz',
         source_amount: { value: '.005', currency: 'XRP', issuer: '' },
@@ -89,15 +85,14 @@ describe('Ripple REST Client sendPayment', function() {
       };
 
       client.sendAndConfirmPayment(paymentObj, function(error, response){
-        console.log('err',  error);
-        console.log('response', response);
         assert(error);
+        assert.strictEqual(error.error_type, 'invalid_request');
         done();
       });
     });
 
   } else {
-    it.skip('should fail because destination account and amount is missing');
+    it.skip('should fail because destination account and amount is missing -- MISSING SECRET, so skipping test');
   }
 
 });
